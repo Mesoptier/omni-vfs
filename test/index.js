591996c2da867c24ae5ae52bf526e16555067930
Koen Klaren <koen.klaren@gmail.com>
2015-12-07 21:36:00 +0100

    Use omni-tree for testing

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@ import chai, { expect } from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import isStream from 'is-stream';
 import streamToPromise from 'stream-to-promise';
+import { tree } from 'omni-tree';
 
 import { OmniBase } from '../lib/index';
 
@@ -17,15 +18,26 @@ export function omniBaseTest({ name, impl, init, mock }) {
 
     before(() => {
       // Install mock data
-      mock.install({
-        'empty-dir': {},
-        'directory': {
-          'a.txt': '',
-          'b.txt': ''
-        },
-        'plain.txt': 'content',
-        'jenny.png': new Buffer([8, 6, 7, 5, 3, 0, 9])
-      });
+      mock.install(tree((f, d, s) => {
+        return {
+          'readdir': d({
+            'empty-dir': d({}),
+            'directory': d({
+              'a.txt': '',
+              'b.txt': ''
+            }),
+          }),
+
+          'readfile': d({
+            'plain.txt': f('content'),
+            'jenny.png': f(new Buffer([8, 6, 7, 5, 3, 0, 9]))
+          }),
+
+          'stat': d({
+            'file.txt': f('content', { mode: '0777' })
+          })
+        };
+      }));
 
       // Initiate the VFS
       vfs = init();
@@ -38,15 +50,15 @@ export function omniBaseTest({ name, impl, init, mock }) {
     describe('#readdir()', () => {
 
       it('should resolve with an array of the directory contents', () => {
-        return expect(vfs.readdir('directory')).to.eventually.become(['a.txt', 'b.txt']);
+        return expect(vfs.readdir('readdir/directory')).to.eventually.become(['a.txt', 'b.txt']);
       });
 
       it('should resolve with an empty array when the directory is empty', () => {
-        return expect(vfs.readdir('empty-dir')).to.eventually.become([]);
+        return expect(vfs.readdir('readdir/empty-dir')).to.eventually.become([]);
       });
 
       it('should be rejected when the directory does not exist', () => {
-        return expect(vfs.readdir('non-existant-dir')).to.be.rejectedWith('ENOENT');
+        return expect(vfs.readdir('readdir/non-existant-dir')).to.be.rejectedWith('ENOENT');
       });
 
     });
@@ -54,13 +66,13 @@ export function omniBaseTest({ name, impl, init, mock }) {
     describe('#readfile()', () => {
 
       it('should resolve with a ReadableStream', () => {
-        return vfs.readfile('plain.txt').then((stream) => {
+        return vfs.readfile('readfile/plain.txt').then((stream) => {
           return expect(isStream.readable(stream)).to.be.true;
         });
       });
 
       it('should resolve with plain text content', () => {
-        return vfs.readfile('plain.txt').then((stream) => {
+        return vfs.readfile('readfile/plain.txt').then((stream) => {
           return streamToPromise(stream).then((buffer) => {
             return expect(buffer.toString()).to.equal('content');
           });
@@ -68,11 +80,19 @@ export function omniBaseTest({ name, impl, init, mock }) {
       });
 
       it('should resolve with buffer content', () => {
-        return vfs.readfile('jenny.png').then((stream) => {
+        return vfs.readfile('readfile/jenny.png').then((stream) => {
           return streamToPromise(stream).then((buffer) => {
             return expect(buffer).to.deep.equal(new Buffer([8, 6, 7, 5, 3, 0, 9]));
           });
         });
+      });
+
+    });
+
+    describe('#stat()', () => {
+
+      it('should resolve with a Stats object', () => {
+        return expect(vfs.stat('stat/file.txt')).to.resolve;
       });
 
     });
